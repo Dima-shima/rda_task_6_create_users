e6ff030c985f66dbea2111134b283ddbca26896e
Dima-shima <shifrin1970@gmail.com>
2026-06-15 00:06:10 +0200

    Edit task.sql

--- task.sql
+++ task.sql
@@ -1,7 +1,6 @@
 USE ShopDB
-
-​​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%';
 
-​​CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL ON database.table TO 'deploymentuser'@'%';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
